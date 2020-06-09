354a7900a2fd1f78ae35ce70a4dede24084a198e
limmens <66556410+limmens@users.noreply.github.com>
2020-06-09 08:42:58 +0200

    bezig met legOpEindStapel; werkt nog niet naar behoren

--- src/patience/AflegStapel.java
+++ src/patience/AflegStapel.java
@@ -12,27 +12,13 @@ package patience;
  */
 public class AflegStapel extends Stapel
 {
-    public AflegStapel()//Kaart k)
+    public AflegStapel()
     {
         nKaarten = 0;
-        //addKaart(k);
     }
     
-    //voegt kaart toe aan aflegstapel en draait hem open
-    public void addKaart(Kaart k)
-    {
-        k.setVisible(true);
-        if(nKaarten > 0)
-            bovensteKaart.setVisible(false);
-            
-        bovensteKaart = k;
-        kaarten[nKaarten] = k;
-        
-        bovensteKaart.setZichtbaar(true);
-        bovensteKaart.verversKaart();
-        this.add(bovensteKaart);
-        
-        nKaarten++;
-    }
+    
+    
+    
 
 }
