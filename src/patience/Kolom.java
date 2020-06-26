994962a2954aa05813b3c026f1f4999ecedec5a9
limmens <66556410+limmens@users.noreply.github.com>
2020-06-26 14:43:24 +0200

    lijkt volledig te werken behalve z-order kolommen

--- src/patience/Kolom.java
+++ src/patience/Kolom.java
@@ -9,21 +9,19 @@ import java.awt.*;
 import javax.swing.*;
 import javax.swing.GroupLayout.*;
 
-
-
 /**
- *
  * @author Loes Immens
  */
 public class Kolom extends Stapel
 {
+    private final int kolomNr;
     
     public Kolom(int n, Kaart[] kaarten)
     {
         super();
         
         nKaarten = n;
-        
+        kolomNr = n;
         
         GroupLayout gl = new GroupLayout(this);
         this.setLayout(gl);
@@ -53,13 +51,69 @@ public class Kolom extends Stapel
             addComponent(kaarten[i]));
             sg.addGroup(gl.createParallelGroup().
             addComponent(kaarten[i]));
-            kaarten[i].repaint();
         }
     }
     
     public Kaart[] getKaarten()
     {
         return kaarten;
+    }
+    
+    public void setAangeklikt(boolean a, Kaart[] aangeklikteKaarten, int nAangeklikt)
+    {
+        aangeklikt = a;
+        if(!aangeklikt)
+        {
+            for(int i = 0; i < nKaarten; i++)
+                kaarten[i].setAangeklikt(false);
+        }
+        else
+        {
+            for(int i = 0; i < nKaarten; i++)
+            {
+                for(int j = 0; j < nAangeklikt; j++)
+                {
+                    if(kaarten[i] == aangeklikteKaarten[j])
+                    {
+                        kaarten[i].setAangeklikt(true);
+                        System.out.println(kaarten[i] + " aangeklikt vanuit " + this);
+                    }
+                }
+            }
+        }
+    }
+    
+    public void toonKolomJuist()
+    {
+        GroupLayout gl = new GroupLayout(this);
+        this.setLayout(gl);
+        
+        ParallelGroup pg = gl.createParallelGroup(GroupLayout.Alignment.LEADING);
+        gl.setHorizontalGroup(gl.createSequentialGroup().addGroup(pg));
+        SequentialGroup sg = gl.createSequentialGroup();
+        gl.setVerticalGroup(sg);
+        
+        if(nKaarten == 0)
+        {
+            pg.addGroup(gl.createSequentialGroup().
+            addComponent(leegLabel));
+            sg.addGroup(gl.createParallelGroup().
+            addComponent(leegLabel));
+        }
+            
+        for(int i = 0; i < nKaarten; i++)
+        {
+            kaarten[i].setY(i * Deck.getHeightUnderlying());
+            kaarten[i].toonJuisteKant();
+            System.out.println("\n" + kaarten[i] + " wordt opnieuw op " + this + " gelegd op positie Y = " + kaarten[i].getY());
+            
+            pg.addGroup(gl.createSequentialGroup().
+            addComponent(kaarten[i]));
+            sg.addGroup(gl.createParallelGroup().
+            addComponent(kaarten[i]));
+            
+            this.setComponentZOrder(kaarten[i], i);
+        }
     }
     
     @Override
@@ -73,4 +127,11 @@ public class Kolom extends Stapel
     {
         return 0;
     }
+    
+    @Override
+    public String toString()
+    {
+        return "Kolom " + kolomNr;
+    }
+    
 }
